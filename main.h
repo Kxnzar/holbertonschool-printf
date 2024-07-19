458ef72429bdd0aa4af7e47cfa17dedfc8d033aa
Gabriel <gabyrivm06@gmail.com>
2024-07-19 14:18:44 -0400

    "Updated main.h with new function declarations, removed unnecessary includes from man_3_printf, deleted files printf/ELLO and test.c, and refactored _printf function in test.c."

--- main.h
+++ main.h
@@ -3,10 +3,17 @@
 #include <stdarg.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _printf(const char *format, ...);
-
-
-
+int _putchar(char c);
+int _print_char(va_list val);
+int _print_string(va_list args);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int _print_format_specifier(void);
+int _print_integer(va_list args);
+int _print_float(va_list args);
 
 #endif
